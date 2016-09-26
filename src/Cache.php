ced96c792975c691600eb3a9431349bccc431836
Martin Adamko <attitude@users.noreply.github.com>
2016-09-26 15:00:16 +0200

    Add force refresh by interval

--- src/Cache.php
+++ src/Cache.php
@@ -8,7 +8,8 @@ namespace RFPL;
 class Cache
 {
     protected $path;
-    protected $ttl;
+    protected $ttl = null;
+    protected $refreshInterval = null;
     protected $url;
     protected $filter = null;
     protected $alreadySent = false;
@@ -17,8 +18,9 @@ class Cache
     public function __construct(array $options = [])
     {
         $defaults = [
-            'path' => 'cache',
-            'ttl' => 300, // seconds to pass request
+            'path'       => 'cache',
+            'ttl'        => 300, // seconds to pass request
+            'refreshInterval' => null, // at each N * seconds to pass request
             'requestUri' => $_SERVER['REQUEST_URI']
         ];
 
@@ -43,6 +45,10 @@ class Cache
 
         if ($ttl >= 0) {
             $this->ttl = (int) $ttl;
+        }
+
+        if ($this->refreshInterval !== null) {
+            $this->refreshInterval = $this->parseCronJobStamp($this->refreshInterval);
         }
 
         $this->url = $_SERVER['HTTP_HOST'].$requestUri;
@@ -95,6 +101,8 @@ class Cache
         });
     }
 
+    /**
+     */
     protected function resetCache()
     {
         $this->reset = true;
@@ -119,34 +127,39 @@ class Cache
 
         // Get cache file path
         if ($file = realpath($this->cacheFile())) {
-            $content = file_get_contents($file);
+            // File modify time
+            $filemtime = filemtime($file);
 
-            if ($filter && is_callable($filter)) {
-                $content = $filter($content);
-            }
+            if (!$this->shouldRefresh($filemtime)) {
+                $content = file_get_contents($file);
 
-            $encoding = array_map('trim', explode(',', @$_SERVER['HTTP_ACCEPT_ENCODING'] ? $_SERVER['HTTP_ACCEPT_ENCODING'] : ''));
+                if ($filter && is_callable($filter)) {
+                    $content = $filter($content);
+                }
 
-            if (in_array('gzip', $encoding)) {
-                $content = gzencode($content);
-                header('Content-Encoding: gzip');
-            }
+                $encoding = array_map('trim', explode(',', @$_SERVER['HTTP_ACCEPT_ENCODING'] ? $_SERVER['HTTP_ACCEPT_ENCODING'] : ''));
 
-            $contentLength = strlen($content);
-            header('Content-Length: '.$contentLength);
-            header('Connection: close');
+                if (in_array('gzip', $encoding)) {
+                    $content = gzencode($content);
+                    header('Content-Encoding: gzip');
+                }
 
-            ob_start();
-            echo $content;
-            ob_end_flush();
-            ob_flush();
-            flush();
+                $contentLength = strlen($content);
+                header('Content-Length: '.$contentLength);
+                header('Connection: close');
 
-            $this->alreadySent = true;
+                ob_start();
+                echo $content;
+                ob_end_flush();
+                ob_flush();
+                flush();
 
-            // Cache is still fresh:
-            if (filemtime($file) + $this->ttl >= time()) {
-                exit();
+                $this->alreadySent = true;
+
+                // Cache is still fresh:
+                if ($this->ttl !== null && $filemtime + $this->ttl >= time()) {
+                    exit();
+                }
             }
         } elseif ($filter) {
             // Store filter and apply it after store procedure
@@ -156,6 +169,136 @@ class Cache
         // Cache served was old, continue with processing
         // store output but don't output anything:
         ob_start([$this, 'store']);
+    }
+
+    /**
+     * Checks whether cache is invalid
+     *
+     * @param int $filemtime File modification time
+     * @return boolean
+     *
+     */
+    protected function shouldRefresh(/*int*/ $filemtime)
+    {
+        if (!$this->refreshInterval) {
+            return false;
+        }
+
+        foreach ($this->refreshInterval['year'] as $year) {
+            foreach ($this->refreshInterval['month_of_the_year'] as $month) {
+                foreach ($this->refreshInterval['day_of_the_month'] as $day) {
+                    foreach ($this->refreshInterval['hour'] as $hour) {
+                        foreach ($this->refreshInterval['minute'] as $minute) {
+                            if ($time = strtotime("{$year}-{$month}-{$day} {$hour}:{$minute}")) {
+                                if (in_array(date('N', $time), $this->refreshInterval['day_of_the_week']) && $time > $filemtime) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Parse Cronjob interval format
+     *
+     * @param string $cronJobTimestamp Interval string
+     * @return array Array of intervals
+     *
+     */
+    protected function parseCronJobStamp($cronJobTimestamp)
+    {
+        $cronJobTimestamp = explode(' ', $cronJobTimestamp);
+
+        if (count($cronJobTimestamp) !== 6) {
+            throw new \Exception("Cron job timestamp expects 6 values", 500);
+        }
+
+        foreach ($cronJobTimestamp as $i => &$v) {
+            if ($v === '*') {
+                switch ($i) {
+                    case 0: $v = [(int) date('i')]; break;
+                    case 1: $v = [(int) date('G')]; break;
+                    case 2: $v = [(int) date('j')]; break;
+                    case 3: $v = [(int) date('n')]; break;
+                    case 4: $v = [(int) date('N')]; break;
+                    case 5: $v = [(int) date('Y')]; break;
+                }
+            } elseif (preg_match('|^(\d+)-(\d+)/(\d+)$|', $v, $m) || preg_match('|^(\d+)-(\d+)$|', $v, $m)) {
+                if ($m[1] >= $m[2]) {
+                    throw new \Exception("First number must be less than the second of the range", 500);
+                }
+
+                $step = 1;
+
+                if (isset($m[3])) {
+                    if ($m[3] < 0) {
+                        throw new \Exception("Range step must be more than zero", 500);
+                    }
+
+                    $step = $m[3];
+                }
+
+                $v = range($m[1], $m[2], $step);
+            } elseif (preg_match('|^[\d,]+$|', $v, $m)) {
+                $v = array_filter(explode(',', $v), function ($v) {
+                    return $v === '0' || (int) $v > 0;
+                });
+            } else {
+                throw new \Exception("Unsupported crojjob format", 500);
+            }
+        }
+
+        $cronValues = [
+            'minute'            => $cronJobTimestamp[0],
+            'hour'              => $cronJobTimestamp[1],
+            'day_of_the_month'  => $cronJobTimestamp[2],
+            'month_of_the_year' => $cronJobTimestamp[3],
+            'day_of_the_week'   => $cronJobTimestamp[4],
+            'year'              => $cronJobTimestamp[5]
+        ];
+
+        foreach ($cronValues['minute'] as $v) {
+            if ($v < 0 || $v > 59) {
+                throw new \Exception("Out of range: {$v} minute", 1);
+            }
+        }
+
+        foreach ($cronValues['hour'] as $v) {
+            if ($v < 0 || $v > 23) {
+                throw new \Exception("Out of range: {$v} hour", 1);
+            }
+        }
+
+        foreach ($cronValues['day_of_the_month'] as $v) {
+            if ($v < 1 || $v > 31) {
+                throw new \Exception("Out of range: {$v} day of the month", 1);
+            }
+        }
+
+        foreach ($cronValues['month_of_the_year'] as $v) {
+            if ($v < 1 || $v > 12) {
+                throw new \Exception("Out of range: {$v} month of the year", 1);
+            }
+        }
+
+        foreach ($cronValues['day_of_the_week'] as $v) {
+            if ($v < 1 || $v > 7) {
+                throw new \Exception("Out of range: {$v} day of the week", 1);
+            }
+        }
+
+        foreach ($cronValues['year'] as $v) {
+            if ($v < 1900 || $v > 3000) {
+                throw new \Exception("Out of range: {$v} year", 1);
+            }
+        }
+
+        return $cronValues;
     }
 
     /**
